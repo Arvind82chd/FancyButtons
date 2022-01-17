466a2b527bb09ad2d38f22100d573c2174bd71e8
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-17 20:52:28 +0000

    lifting state up for LightSwitchButton and app

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
+  const [light, setLight] = useState('off');
+
   return (
     <div className={ 'App' }>
     <h1>Fancy Buttons!</h1>  
     <section>
       <AngryButton />
       <CounterButton />
-      <LightSwitchButton />
+      <LightSwitchButton light={light} setLight={setLight} />
       <TextRepeaterButton />
       </section>    
     </div>
